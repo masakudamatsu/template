0be967587ee9366b34c3c9caeace7c6e49fe9c2f
Unknown <m.kudamatsu@gmail.com>
2019-05-07 15:30:16 +0900

    jQuery for manipulating HTML #21

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,9 @@
 // jQuery
 $(document).ready(function() {
+  // Append a new HTML element
+  var $parentElement = $('.CLASS');
+  var newText = "TEXT";
+  var $childElement = $('<TAG>' + newText + '</TAG>');
+  $parentElement.append($childElement);
   // Here write event handers
 });
